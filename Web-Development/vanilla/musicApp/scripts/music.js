0bada9790a6a2e1a38d8249747657276420f0b79
Himesh <y.himesh026@gmail.com>
2024-05-12 13:05:16 +0530

    Removed all data related to the deleted musics

--- Web-Development/vanilla/musicApp/scripts/music.js
+++ Web-Development/vanilla/musicApp/scripts/music.js
@@ -1,23 +1,5 @@
 let musicArray = [
   {
-  title:"Aathma Rama",
-  path:"./source/music/Aathma Rama.mp3",
-  image:"./source/image/Aathma Rama.jpg",
-  artist:"Brodha V",
-  },
-  {
-    title:"Bhole Charniya Aaradhana",
-    path:"./source/music/Bhole Charniy Aaradhana.mp3",
-    artist:"Unknown",
-    image:"",
-  },
-  {
-    title:"Habibi Remix",
-    path:"./source/music/Habibi Remix.mp3",
-    artist:"",
-    image:"./source/image/Habibi Remix.jpg"
-  },
-  {
     title:"Unakku Thaan",
     path:"./source/music/Unakku Thaan.mp3",
     artist:"",
